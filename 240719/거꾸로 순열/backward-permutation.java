9469386479687a30c06e62e1fa77e97c07b1abf1
KSH_coding <seongha.dev@gmail.com>
2024-07-19 16:18:40 +0900

    Title: 거꾸로 순열, Time: 1065ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240719/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n + 1];
+
+        back(0, new int[n]);
+    }
+
+    private static void back(int depth, int[] choice) {
+        if (depth == n) {
+            for (int num : choice) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = n; i > 0; i--) {
+            if (!visited[i]) {
+                visited[i] = true;
+                choice[depth] = i;
+                back(depth + 1, choice);
+                visited[i] = false;
+            }
+        }
+    }
+}
