58e9bcde2cc4213716de959acbf7182efe5c2594
KSH_coding <seongha.dev@gmail.com>
2024-07-23 11:55:01 +0900

    Title: n개의 점 중 m개 고르기, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240723/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static int[][] map;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer input = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(input.nextToken());
+        m = Integer.parseInt(input.nextToken());
+        map = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer xyInput = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(xyInput.nextToken());
+            int y = Integer.parseInt(xyInput.nextToken());
+            map[i][0] = x;
+            map[i][1] = y;
+        }
+
+        back1(0, 0, new int[m]);
+        System.out.println(min);
+    }
+
+    private static void back1(int depth, int start, int[] choice) {
+        if (depth == m) {
+            back2(0, 0, choice, new int[2]);
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            choice[depth] = i;
+            back1(depth + 1, i + 1, choice);
+        }
+    }
+
+    private static void back2(int depth, int start, int[] choice1, int[] choice2) {
+        if (depth == 2) {
+            int x1 = map[choice1[choice2[0]]][0];
+            int x2 = map[choice1[choice2[1]]][0];
+            int y1 = map[choice1[choice2[0]]][1];
+            int y2 = map[choice1[choice2[1]]][1];
+
+            int xResult = (x1 - x2) * (x1 - x2);
+            int yResult = (y1 - y2) * (y1 - y2);
+            int result = xResult + yResult;
+
+            min = Math.min(min, result);
+            return;
+        }
+
+        for (int i = start; i < choice1.length; i++) {
+            choice2[depth] = i;
+            back2(depth + 1, i + 1, choice1, choice2);
+        }
+    }
+}
